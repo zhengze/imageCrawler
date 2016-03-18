876bfca0bb6d54615068ed1531d12886787229e0
zhengze <zhang_zhze@sina.com>
2016-03-18 17:15:56 +0800

    implement threading task

---
+++ crawler.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+#coding:utf8
+
+'''
+Script Name: imagecrawler.py
+Author: zhanghai
+Created: 2016-03-15
+Last Modified: 
+Version: 1.0
+Description: crawl images from http://wwww.meizitu.com
+'''
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import os
+import time
+from multiprocessing import Pool, Process
+import signal
+import sys
+import logging
+from task import UrlThread, DownloadThread
+from config import *
+
+logger = logging.getLogger(__name__)
+
+
+def get_page_links1(webUrl):
+    '''
+        Description: get images' links
+    '''
+    webList = []
+    htmlContent = requests.get(webUrl)
+    soup = BeautifulSoup(htmlContent.text, "html.parser")
+    wp_page_numbers_div = soup.find("div", {"id":"wp_page_numbers"})
+    endPageTag = wp_page_numbers_div.find_all("a")[-1]
+    endPageLink = endPageTag.get('href')
+    if endPageLink: 
+        regex = r"(\D+\d+\D+)(\d+)(\D+)"
+        m = re.match(regex, endPageLink)
+        if m:
+            pageNumber = int(m.groups()[1])  #get page number
+            pageLinks = []
+            for index in xrange(1, pageNumber+1):
+                pageLink = "%s"*4 %(webUrl, m.group(1), index, m.group(3))
+                #pageLinks.append(pageLink)
+                page_link1_queue.put(pageLink)
+            return page_link1_queue
+        else:
+            return None
+
+def get_page_links2():
+    pageLink = page_link1_queue.get()
+    print "page links2 process id:%s" %os.getpid()
+    print "Starting to crawl : %s" %pageLink
+    if pageLink:
+        picture_urls = []  
+        response = requests.get(pageLink) 
+        soup = BeautifulSoup(response.text, "html.parser")
+        picture_divs = soup.find_all("div", {"class":"pic"})
+        for picture_div in picture_divs:
+            picture_url = picture_div.find("a").get("href")
+            #picture_urls.append(picture_url)
+            page_link2_queue.put(picture_url)
+        return page_link2_queue
+    else:
+        return None
+
+def link2_middleware():
+    '''middleware for get link of images html'''
+    print '**************link2_middleware*************'
+    while 1:
+        time.sleep(1)
+        get_page_links2()
+
+def download_images():
+    images_url = page_link2_queue.get()
+    print "download process id: %s" %os.getpid()
+    print "Starting to crawl : %s" %images_url
+    if images_url:
+        response = requests.get(images_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        image_div = soup.find("div", {"id":"picture"})
+        image_links = image_div.find_all("img")
+        images_names = []
+        for image_link in image_links:
+            image_source = image_link.get("src")
+            regex = r"(\D+)(\d+)(\D+)(\d+)(\D+)(\d+)(\D+)(\d+)(\D+)"
+            m = re.match(regex, image_source)
+            if m:
+                image_name = "%s_%s_%s_%s%s" %(m.group(2), m.group(4), \
+                    m.group(6), m.group(8), m.group(9)) 
+                r = requests.get(image_source, stream=True)
+
+                #download images
+                fname = os.path.join(DOWNLOAD_DIR, image_name)
+                if not os.path.exists(fname):
+                    with open(fname, "wb") as fd:
+                        for chunk in r.iter_content():
+                            fd.write(chunk)
+                    print "%s has been downloaded" %image_name
+                else:
+                    continue
+            else:
+                return None
+
+def download_middleware():
+    '''middleware for download images'''
+    print '***********download_middleware***********'
+    while 1:
+        time.sleep(1)
+        download_images()
+
+
+def thread_task():
+    '''
+    pageLinks = get_page_links1(HOST)
+    for index in xrange(len(pageLinks)):
+        print "Starting to crawl page: %s" %(index+1)
+        image_urls = get_page_links2(pageLinks[index])
+        for image_url in image_urls:
+            download_images(image_url)
+    '''
+
+    for i in xrange(THREAD_NUM):
+        url_thread = UrlThread(get_page_links2)
+        url_thread.setDaemon(True)
+        url_thread.start()
+
+    for i in xrange(THREAD_NUM):
+        download_thread = DownloadThread(download_images)
+        download_thread.setDaemon(True)
+        download_thread.start()
+
+    for i in xrange(THREAD_NUM):
+        url_thread.join()
+
+    for i in xrange(THREAD_NUM):
+        download_thread.join()
+        
+
+def process_task():
+    '''
+    subprocess for crawl_main
+    '''
+    try:
+        get_page_links1(HOST)
+
+        '''
+        processes = []
+        for i in xrange(PROCESS_NUM):
+            download_process = Process(target=download_images)
+            download_process.start()
+            processes.append(download_process)
+
+        for p in processes:
+            p.join()
+
+        pool1 = Pool(PROCESS_NUM)
+        for i in xrange(PROCESS_NUM):
+            pool1.apply_async(link2_middleware)
+
+        pool2 = Pool(PROCESS_NUM)
+        for i in xrange(PROCESS_NUM):
+            pool2.apply_async(download_middleware)
+
+        pool1.close()
+        pool1.join()
+        pool2.close()
+        pool2.join()
+        '''
+
+        pool = Pool(PROCESS_NUM)
+        for i in xrange(PROCESS_NUM):
+            pool.apply_async(thread_task)
+        pool.close()
+        pool.join()
+
+        print 'All work done.'
+    except KeyboardInterrupt:
+        sys.exit(0)
+    
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, process_task)
+    signal.signal(signal.SIGTERM, process_task)
+
+    start = time.time()
+    process_task()
+
+    end = time.time()
+    running_time = end - start
+    print "The programe's running time:%s" %running_time
+
+
