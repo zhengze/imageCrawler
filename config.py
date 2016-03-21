5af2f2cef7b2431e6a433eaaa3522c065238c255
zhengze <zhang_zhze@sina.com>
2016-03-21 19:04:53 +0800

    --long

--- config.py
+++ config.py
@@ -16,3 +16,9 @@ PROCESS_NUM = 2
 manager = Manager()
 page_link1_queue = manager.Queue()
 page_link2_queue = manager.Queue()
+
+SECRET_KEY = 'm#ovtr^ax%n3)m$80s6-5x*$^3-izig&ghfccuohb5$(851l&s'
+
+DATA_DIR = os.path.join(BASE_DIR, "data")
+SQLALCHEMY_DATABASE_URI = "%s%s%s" %("sqlite:///", DATA_DIR, "/meizitu.db")
+SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, "db_repository")
