308d28e4528356bb72f9af3e65da02e3bb922f46
zhengze <zhang_zhze@sina.com>
2016-03-21 19:11:18 +0800

    add sqlalchemy migrate scripts

---
+++ db_create.py
@@ -0,0 +1,12 @@
+from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO
+from models import init_db
+from migrate.versioning import api
+import os.path
+
+init_db()
+
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
